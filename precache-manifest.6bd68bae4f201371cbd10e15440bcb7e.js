2be9b9a6e839f18fd41cdbb66ee0d2dc603af7e4
Cristiane Mayara <cris.mayara@gmail.com>
2019-03-28 01:38:59 -0300

    Updates

---
+++ precache-manifest.6bd68bae4f201371cbd10e15440bcb7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "535557b3b43044d46ee0",
+    "url": "/static/js/main.535557b3.chunk.js"
+  },
+  {
+    "revision": "b983acc355ba6bdc1b67",
+    "url": "/static/js/1.b983acc3.chunk.js"
+  },
+  {
+    "revision": "535557b3b43044d46ee0",
+    "url": "/static/css/main.f7ce91c8.chunk.css"
+  },
+  {
+    "revision": "407af42daa6e5faa5ffab5f8bee094ce",
+    "url": "/index.html"
+  }
+];
