2c7f438ce520fbb1768be0d313cd6c5c8eeb3e68
Cristiane Mayara <cris.mayara@gmail.com>
2019-08-11 19:13:57 -0300

    Refinement on components

--- project/src/components/ContactUs.js
+++ project/src/components/ContactUs.js
@@ -1,20 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class ContactUs extends Component {
-  render() {
-    let resumeData = this.props.resumeData;
-
-    return (
-      <section id="contact">
-        <div className="row section-head">
-          <div className="ten columns">
-            <p className="lead">
-              Sinta-se à vontade para entrar em contato comigo para algum
-              trabalho ou sugestão
-            </p>
-          </div>
-        </div>
-        {/* <div className="row">
+const ContactUs = ({ resumeData }) => (
+  <section id="contact">
+    <div className="row section-head">
+      <div className="ten columns">
+        <p className="lead">
+          Sinta-se à vontade para entrar em contato comigo para algum trabalho
+          ou sugestão
+        </p>
+      </div>
+    </div>
+    {/* <div className="row">
           <aside className="eigth columns footer-widgets">
             <div className="widget">
               <h4>Linked in :
@@ -23,7 +19,7 @@ export default class ContactUs extends Component {
             </div>
           </aside>
         </div> */}
-      </section>
-    );
-  }
-}
+  </section>
+);
+
+export default ContactUs;
--- project/src/components/Footer.js
+++ project/src/components/Footer.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
-export default class Footer extends Component {
-  render() {
-    let resumeData = this.props.resumeData;
-    return (
-      <footer>
-      <div className="row">
-        <div className="twelve columns">
-          <ul className="social-links">
-            {
-              resumeData.socialLinks && resumeData.socialLinks.map((item)=>{
-                return(
-                  <li>
-                    <a href={item.url}>
-                    <i className={item.className} />
-                    </a>
-                  </li>
-                )
-              })
-            }
-          </ul>
-          
-        </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
+import React from "react";
+
+const Footer = ({ resumeData }) => (
+  <footer>
+    <div className="row">
+      <div className="twelve columns">
+        <ul className="social-links">
+          {resumeData.socialLinks &&
+            resumeData.socialLinks.map(item => (
+              <li>
+                <a href={item.url}>
+                  <i className={item.className} />
+                </a>
+              </li>
+            ))}
+        </ul>
       </div>
-    </footer>
-    );
-  }
-}
+      <div id="go-top">
+        <a className="smoothscroll" title="Back to Top" href="#home">
+          <i className="icon-up-open" />
+        </a>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
